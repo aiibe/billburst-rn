e9abd54410aa50b784b912c2c177b7aa37fb6d69
aiibe <souksavanh.sy@gmail.com>
2022-02-14 10:46:32 +0100

    Add button linked to expense screen modal

--- screens/types/Navigation.ts
+++ screens/types/Navigation.ts
@@ -3,4 +3,5 @@ import { ParamListBase } from "@react-navigation/native";
 export interface RootStackParamsList extends ParamListBase {
   Overview: undefined;
   Friend: { name: string };
+  AddExpense: undefined;
 }
